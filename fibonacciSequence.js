2054ad4ac65ec523d7a80c66e81de3cb1d9b7e01
sabbirlog <mridhasabbir5@gmail.com>
2024-03-19 15:39:15 +0600

    feat: recursive solution of fibonacci sequence

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -1,13 +1,36 @@
+/**
+ * Iterative solution
+ * The fibonacci sequence is a sequence in which each number is the sum of two preceding ones
+ * linear time complexity O(n). It's depend on n or you could say input size.
+ * BigO = O(n)
+ * O(n + 2)
+ */
 function fibonacciSeq(n) {
   const assumingFeb = [0, 1];
   for (let i = 2; i < n; i++) {
-    /**
-     * linear time complexity O(n). It's depend on n or you could say input size.
-     * O(n + 2)
-     */
     assumingFeb[i] = assumingFeb[i - 2] + assumingFeb[i - 1];
   }
   return assumingFeb;
 }
 
-console.log(fibonacciSeq(6));
+/**
+ * recursive solution
+ * we need to keep in mind. when use recursive technique, need a base case to terminate the infinite loop.
+ * We need to find out smaller chunk of the same problem  and write code to solve smaller problem.
+ * F of n = F of (n - 1) + F of (n - 2).
+ * Base case: if F of n = 0, return 0. if F of n = 1, return 1.
+ * BigO = O(2^n)
+ */
+
+function recursiveFibonacciSeq(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return recursiveFibonacciSeq(n - 1) + recursiveFibonacciSeq(n - 2);
+}
+
+console.log(recursiveFibonacciSeq(0));
+console.log(recursiveFibonacciSeq(1));
+console.log(recursiveFibonacciSeq(6));
+console.log(recursiveFibonacciSeq(7));
